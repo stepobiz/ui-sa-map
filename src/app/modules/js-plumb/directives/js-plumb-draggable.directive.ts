c470ec4e260ac8b28ec9b62e67d590653815514f
stepobiz <stefano@stepo.biz>
2019-06-09 12:11:00 +0200

    directive

--- src/app/modules/js-plumb/directives/js-plumb-draggable.directive.ts
+++ src/app/modules/js-plumb/directives/js-plumb-draggable.directive.ts
@@ -3,14 +3,20 @@ import { JsPlumbService } from '../js-plumb.service';
 
 @Directive({
   selector: '[jsplumb-draggable]',
-  
 })
 export class JsPlumbDraggableDirective {
-    constructor(elementRef: ElementRef, jsPlumbService: JsPlumbService) {
-        setTimeout(function(){
-            jsPlumbService.getInstance().draggable(elementRef.nativeElement.id, {
-                grid: [10, 10]
-            });
-        }, 0);
-    }   
+  elementRef: ElementRef;
+
+  constructor(elementRef: ElementRef, jsPlumbService: JsPlumbService) {
+    this.elementRef = elementRef;
+    setTimeout(function(){
+      jsPlumbService.getInstance().draggable(elementRef.nativeElement.id, {
+        grid: [10, 10]
+      });
+    }, 0);
+  }   
+
+  @HostListener('click') onMouseLeave() {
+    console.log("lota", this.elementRef.nativeElement.style.top);
+  }
 }
